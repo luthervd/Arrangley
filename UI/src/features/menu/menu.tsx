914b39e60e3aeccbcaf113cbe1be3e1d7f254a40
luthervd <luke.mcg79@gmail.com>
2023-10-29 22:40:39 +0000

    Need to change custom styling for bulma naming

--- UI/src/features/menu/menu.tsx
+++ UI/src/features/menu/menu.tsx
@@ -12,8 +12,8 @@ export default function Menu(props: any)
                 Arrangley
             </p>
             <ul className="menu-list">
-                <li><a className={menuItem === menuItems.tasks ? 'is-active' : ''} onClick={evt => dispatch(selectMenuItem(menuItems.tasks))}>Tasks</a></li>
-                <li><a className={menuItem === menuItems.createItem ? 'is-active' : ''} onClick={evt => dispatch(selectMenuItem(menuItems.createItem))}>Create Item</a></li>
+                <li><a className={menuItem === menuItems.tasks ? 'is-active has-background-grey-dark' : ''} onClick={evt => dispatch(selectMenuItem(menuItems.tasks))}>Tasks</a></li>
+                <li><a className={menuItem === menuItems.createItem ? 'is-active has-background-grey-dark' : ''} onClick={evt => dispatch(selectMenuItem(menuItems.createItem))}>Create Item</a></li>
             </ul>
         </aside>
     )
