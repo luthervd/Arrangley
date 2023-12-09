89499819bf5473a5b28107d44ba9178fd0fe0cf5
luthervd <luke.mcg79@gmail.com>
2023-12-09 10:12:31 +0000

    USe env var for db migrations

--- serversetup.sh
+++ serversetup.sh
@@ -3,4 +3,4 @@ rm -r ./Server;
 unzip ./serverbuild.zip -d ./;
 chmod 777 efbundle;
 touch appsettings.json;
-echo "{\"ConnectionStrings\": {\"TodoContext\": \"User ID=postgres;Password=postgres;Host=localhost;Port=5433;Database=Organize;\"}}" | cat > appsettings.json;
+echo "{\"ConnectionStrings\": {\"TodoContext\": \"User ID=postgres;Password=$DBP;Host=localhost;Port=5432;Database=Organize;\"}}" | cat > appsettings.json;
