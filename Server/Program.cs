4f98d3ffcf5d46d1265c9712f264c2e689a016ae
luthervd <luke.mcg79@gmail.com>
2024-06-10 16:59:49 +0100

    Temp while pull

--- Server/Program.cs
+++ Server/Program.cs
@@ -2,6 +2,7 @@ using OrganizeApi.Extensions;
 using OrganizeApi.Todo;
 using IdentityModel.Client;
 using System.Security.Claims;
+using Microsoft.OpenApi.Models;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.RegisterServices(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
@@ -20,6 +21,35 @@ builder.Services.AddCors(options =>
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "ArrangelyApi", Version = "v1" });
+     c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+      {
+        Description = @"JWT Authorization header using the Bearer scheme.
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      Example: 'Bearer 12345abcdef'",
+         Name = "Authorization",
+         In = ParameterLocation.Header,
+         Type = SecuritySchemeType.ApiKey,
+         Scheme = "Bearer"
+       });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+      {
+        {
+          new OpenApiSecurityScheme
+          {
+            Reference = new OpenApiReference
+              {
+                Type = ReferenceType.SecurityScheme,
+                Id = "Bearer"
+              },
+              Scheme = "oauth2",
+              Name = "Bearer",
+              In = ParameterLocation.Header,
+
+            },
+            new List<string>()
+          }
+        });
 }); 
 
 
